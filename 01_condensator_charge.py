5ec9eb5025468c59daecea64d5c216aeeeddf4dd
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-03-03 00:01:05 +0100

    Update 01_condensator_charge.py

--- 01_condensator_charge.py
+++ 01_condensator_charge.py
@@ -18,14 +18,14 @@ def change_values(t, state, system):
     return U0 * (1 - np.e**(-t/(R*C)))
 
 def store_results(system, change_values):
-    t_array = modsim.linrange(system.t_0, system.t_end, system.dt)
-    n = len(t_array)
+    times_array = modsim.linrange(system.t_0, system.t_end, system.dt)
+    n = len(times_array)
 
-    series = modsim.TimeSeries(index=t_array)
+    series = modsim.TimeSeries(index=times_array)
     series.iloc[0] = system.t_0
 
     for i in range(n-1):
-        t = t_array[i+1]
+        t = times_array[i+1]
         state = series.iloc[i]
         series.iloc[i+1] = change_values(t, state, system)
     return series
