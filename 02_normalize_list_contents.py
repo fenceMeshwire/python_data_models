26c3d2dc155459ca949fb5b7c6282e6da87c1643
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-07-12 10:17:17 +0200

    Update 02_normalize_list_contents.py

--- 02_normalize_list_contents.py
+++ 02_normalize_list_contents.py
@@ -12,17 +12,22 @@ def normalize_list_contents(list_contents):
     for i in range(len(list_contents)):
         if ',' in list_contents[i]:
             remainder += list_contents[i].split(',')
-
+    if remainder == None: 
+        input_list = unify_sort_list(input_list)
+        return input_list
     list_contents += remainder
 
     for i in range(len(list_contents)):
         if not ',' in list_contents[i]:
             result.append(list_contents[i])
-
-    result = list(set(result))
-    result.sort()
-    result = sorted(result, key=len)
-
+    result = unify_sort_list(result)
+    
+    return result
+    
+def unify_sort_list(input_list):
+    input_list = list(set(input_list))
+    input_list.sort()
+    result = sorted(input_list, key=len)
     return result
 
 if __name__ == '__main__':
